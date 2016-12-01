521541d5ff36f1b4a313c6a46d004c5ce0accfbe
asdfasdf <asdfasdfasdf@asdfasdfs-imac.local>
2016-12-01 20:35:28 +0100

    .

--- Example/KZPlayground/NativeAudioAsset.h
+++ Example/KZPlayground/NativeAudioAsset.h
@@ -15,6 +15,7 @@ typedef void(^CompleteCallback)(NSString*);
 @interface NativeAudioAsset : NSObject  {
 	AVAudioPlayerNode* player;
 	AVAudioPCMBuffer* PCMBuffer;
+    AVAudioUnitTimePitch* pitcher;
 	NSString* audioId;
 	CompleteCallback finished;
 	NSNumber *initialVolume;
